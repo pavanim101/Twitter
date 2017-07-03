0830e62ae5b3f68011bd10949029a98f57c44d88
Pavani Malli <pavanim@fb.com>
2017-07-03 16:40:17 -0700

    Loaded twitter data into the UI elements of each tweet

--- twitter_alamofire_demo/TweetCell.swift
+++ twitter_alamofire_demo/TweetCell.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import AlamofireImage
+
 
 class TweetCell: UITableViewCell {
     
     @IBOutlet weak var tweetTextLabel: UILabel!
     
+    @IBOutlet weak var nameLabel: UILabel!
+        
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var timestampLabel: UILabel!
+    
+    @IBOutlet weak var retweetCountLabel: UILabel!
+    
+    @IBOutlet weak var favoriteCountLabel: UILabel!
+    
     var tweet: Tweet! {
         didSet {
             tweetTextLabel.text = tweet.text
+            nameLabel.text = tweet.user.name
+            usernameLabel.text = "@" + tweet.user.username
+            retweetCountLabel.text = String(tweet.retweetCount)
+            favoriteCountLabel.text = String(tweet.favoriteCount)
+            timestampLabel.text = tweet.createdAtString
+            
+            let profileURL = URL(string: tweet.user.profileImageURL)!
+            print("setting image")
+            print(profileURL)
+            profileImageView.af_setImage(withURL: profileURL)
+            
         }
     }
     
